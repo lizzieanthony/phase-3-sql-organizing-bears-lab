793449e5f27fd34255f2dc62f617af8f4c5a880c
Lizzie <lizziewrab@me.com>
2022-07-12 17:08:03 -0600

    done

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 45, 'M', 'brown', 'irratable', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 45, 'M', 'brown', 'irratable', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 45, 'F', 'brown', 'irratable', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 45, 'M', 'brown', 'irratable', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 45, 'F', 'brown', 'irratable', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 45, 'M', 'brown', 'irratable', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 45, 'F', 'brown', 'irratable', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 45, 'M', 'brown', 'irratable', 1);
